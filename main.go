311113a86efc3c84103b369f9a3ab153a177ef52
Tengku rafi <t.mrr39@gmail.com>
2023-03-09 21:18:02 +0700

    deleted:    handlers/validate_payment.go 	modified:   main.go

--- main.go
+++ main.go
@@ -67,7 +67,6 @@ func main() {
 	v1.GET("/user", middlewares.RequireAuth, handlers.GetUser)
 	v1.GET("/user/subscribe", middlewares.RequireAuth, payment_Handler.PremiumPayment)
 	v1.POST("/subscribe/validate", payment_Handler.PremiumPaymentValidate)
-	// v1.POST("/subscribe/validate", handlers.ValidatePayment)
 
 	v1.GET("/category", category_Handler.GetCategoryHandler)
 	v1.GET("/category/:id", category_Handler.GetCategoryByIdHandler)
