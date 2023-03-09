7e82157e8cdd261ac94f1893fa873a02bc521540
Tengku rafi <t.mrr39@gmail.com>
2023-03-09 11:00:00 +0700

    modified:   main.go

--- main.go
+++ main.go
@@ -66,7 +66,7 @@ func main() {
 
 	v1.GET("/user", middlewares.RequireAuth, handlers.GetUser)
 	v1.GET("/user/subscribe", middlewares.RequireAuth, payment_Handler.PremiumPayment)
-	v1.POST("/user/subscribe/validate", middlewares.RequireAuth, payment_Handler.PremiumPaymentValidate)
+	v1.POST("/user/subscribe/validate", payment_Handler.PremiumPaymentValidate)
 
 	v1.GET("/category", category_Handler.GetCategoryHandler)
 	v1.GET("/category/:id", category_Handler.GetCategoryByIdHandler)
